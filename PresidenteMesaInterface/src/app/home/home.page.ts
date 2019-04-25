a8533f403919bf4970510c2de39d863562d6bafa
sam17332 <32148906+sam17332@users.noreply.github.com>
2019-04-24 18:29:30 -0600

    se agrega un toggle para verificar si se desea habilitar el voto

--- PresidenteMesaInterface/src/app/home/home.page.ts
+++ PresidenteMesaInterface/src/app/home/home.page.ts
@@ -1,20 +1,51 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { AlertController, ActionSheetController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage 
+{
   emailUser = null;
 
-  constructor(
-    private ActivatedRoute: ActivatedRoute
-  ) { }
+  constructor( private ActivatedRoute: ActivatedRoute, private alertController: AlertController, private actionSheet: ActionSheetController) { 
+  }
 
   ngOnInit() {
     //this.argument = this.activatedRoute.snapshot.paramMap.get('id');
     this.emailUser = this.ActivatedRoute.snapshot.paramMap.get('id');
+  }
+
+  toggleValue: boolean = false;
+  async presentAlert() {
+    const alert = await this.alertController.create({
+        header: 'Alerta',
+        message: 'Esta seguro que desea Activar el Voto?',
+        buttons: 
+        [
+        {
+          text: "Cancel",
+          role: 'cancel',
+          handler: () => 
+          {
+            console.log('Yo')
+          }
+        },
+        {
+          text: 'Aceptar',
+          cssClass: 'Secondary',
+          handler: () => 
+          {
+            console.log('Second Handler');
+          }
+        }
+      ]
+    });
+    if(this.toggleValue == true){
+      await alert.present();
+    }
   }
 }
