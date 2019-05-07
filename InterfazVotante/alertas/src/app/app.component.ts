7f6054e555ac1a0cb7dbf827ef09eaabb023abda
GuilleLink <32148569+GuilleLink@users.noreply.github.com>
2019-05-07 17:54:19 -0600

    Loading screen

--- InterfazVotante/alertas/src/app/app.component.ts
+++ InterfazVotante/alertas/src/app/app.component.ts
@@ -4,11 +4,17 @@ import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
+import { timer } from 'rxjs';
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html'
 })
 export class AppComponent {
+
+
+  showSplash = true;
+
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
@@ -21,6 +27,8 @@ export class AppComponent {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
+
+      timer(3000).subscribe(() => this.showSplash = false)
     });
   }
 }
