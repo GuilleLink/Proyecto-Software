947081bcf02d45ab28cbe33b9d8c7f05f25ad4a2
tej17584 <tej17584@uvg.edu.gt>
2019-05-21 07:39:30 -0600

    Cambios de Indentado y Sintaxis
    Cambios regulares, no se asusten.

--- InterfazVotante/alertas/src/app/home/home.page.ts
+++ InterfazVotante/alertas/src/app/home/home.page.ts
@@ -1,56 +1,59 @@
-import { Component, OnInit } from '@angular/core';
-import {AlertController} from '@ionic/angular';
-import {ActionSheetController} from '@ionic/angular';
-import {TaskI} from '../models/task.interface';
-import { InfoService } from '../services/info.service';
+import { Component, OnInit } from "@angular/core";
+import { AlertController } from "@ionic/angular";
+import { ActionSheetController } from "@ionic/angular";
+import { TaskI } from "../models/task.interface";
+import { InfoService } from "../services/info.service";
 
-import { Router } from '@angular/router';
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  selector: "app-home",
+  templateUrl: "home.page.html",
+  styleUrls: ["home.page.scss"]
 })
-export class HomePage implements OnInit{
+export class HomePage implements OnInit {
   candidatos: TaskI[];
-  constructor(private alertController: AlertController, private actionSheet: ActionSheetController,private infoService: InfoService, private router: Router){
+  constructor(
+    private alertController: AlertController,
+    private actionSheet: ActionSheetController,
+    private infoService: InfoService,
+    private router: Router
+  ) {}
 
-  }
-  
-  go(){
-    this.router.navigate(['dlista-nacional'])
+  go() {
+    this.router.navigate(["dlista-nacional"]);
   }
 
-  ngOnInit(){
-    this.infoService.getCandidatos().subscribe((res)=> {
-      console.log('Candidatos',res);
+  ngOnInit() {
+    this.infoService.getCandidatos().subscribe(res => {
+      console.log("Candidatos", res);
       this.candidatos = res;
     });
   }
 
-  async presentAlert(){
-      const alert = await this.alertController.create({
-        header: 'Su voto está a punto de ser enviado',
-        subHeader: 'Sandra Torres',
-        message: '¿Está seguro que desea votar por este candidato?',
-        buttons: [{
-          text: 'Cancel',
-          role: 'cancel',
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      header: '"Su voto está a punto de ser enviado"',
+      subHeader: "Sandra Torres",
+      message: "¿Está seguro que desea votar por este candidato?",
+      buttons: [
+        {
+          text: "Cancel",
+          role: "cancel",
           handler: () => {
-            console.log('You clicked cancel');
+            console.log("You clicked cancel");
           }
-        },            
-          {
-            text: 'Continuar',
-            cssClass: 'secondary',
-            handler: () => {
-              console.log('Second Handler'), this.go();
-            } 
+        },
+        {
+          text: "Continuar",
+          cssClass: "secondary",
+          handler: () => {
+            console.log("Second Handler"), this.go();
           }
-            
-        ]
-      });
+        }
+      ]
+    });
 
-      await alert.present();
+    await alert.present();
   }
 }
