3ad8895416c46c9ce97369c33cc99d133b5ebbbd
Rodrigo Samayoa <toti1998sama@gmail.com>
2019-07-23 22:46:29 -0600

    no message

--- PresidenteMesaInterface/src/app/secretaria/interfaz/interfaz.page.ts
+++ PresidenteMesaInterface/src/app/secretaria/interfaz/interfaz.page.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { AlertController, ActionSheetController } from '@ionic/angular';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-interfaz',
   templateUrl: './interfaz.page.html',
   styleUrls: ['./interfaz.page.scss'],
 })
+
 export class InterfazPage implements OnInit {
 
   constructor(private alertController: AlertController) { 
@@ -16,9 +17,9 @@ export class InterfazPage implements OnInit {
 
   async alerta1() {
     const alert = await this.alertController.create({
-      header: 'Alert',
-      message: 'Esta persona si está empadronada.',
-      buttons: ['Aceptar']
+      header: 'Informacion votante',
+      //message: 'Esta persona si está empadronada. - holaaa',
+      buttons: ['Aceptar'],
     });
     await alert.present();
   }
