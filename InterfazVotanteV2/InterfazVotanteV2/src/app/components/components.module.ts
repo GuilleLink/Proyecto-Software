cafb04396f7d4266c710c08ab806deaaebed2dfd
Alejandro Tejada <tej17584@uvg.edu.gt>
2019-09-29 16:14:41 -0600

    Exports de componentes

--- InterfazVotanteV2/InterfazVotanteV2/src/app/components/components.module.ts
+++ InterfazVotanteV2/InterfazVotanteV2/src/app/components/components.module.ts
@@ -1,13 +1,13 @@
-import { NgModule } from "@angular/core";
-import { CommonModule } from "@angular/common";
-import { HeaderComponent } from "./header/header.component";
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header/header.component';
 import { IonicModule } from '@ionic/angular';
 import { CardComponent } from './card/card.component';
 import { CardNullComponent } from './card-null/card-null.component';
 
 @NgModule({
   declarations: [HeaderComponent],
-  exports: [HeaderComponent,CardComponent, CardNullComponent],
+  exports: [HeaderComponent, CardComponent, CardNullComponent],
   imports: [CommonModule, IonicModule]
 })
-export class ComponentsModule {}
+export class ComponentsModule { }
