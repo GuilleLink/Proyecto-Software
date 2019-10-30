bdfa3bd98676241709de8f6f32efc6cef4120f78
Andres Urizar <uri17632@uvg.edu.gt>
2019-10-30 08:27:25 -0600

    Pagina de confirmacion de voto
    Pagina donde se mostrarn los candidatos seleccionados y se confirmara si quiere realmente votar por ellos.

---
+++ InterfazVotanteV2/InterfazVotanteV2/src/app/pages/confirmacion-voto/confirmacion-voto.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-confirmacion-voto',
+  templateUrl: './confirmacion-voto.page.html',
+  styleUrls: ['./confirmacion-voto.page.scss'],
+})
+export class ConfirmacionVotoPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
