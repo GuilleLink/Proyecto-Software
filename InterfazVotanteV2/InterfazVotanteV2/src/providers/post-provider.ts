eb5df363d5dc9073acae69592f261ca72cc540e9
Gustavo de Leon <gdeleontobias@gmail.com>
2019-11-04 16:21:47 -0600

    arreglo error

--- InterfazVotanteV2/InterfazVotanteV2/src/providers/post-provider.ts
+++ InterfazVotanteV2/InterfazVotanteV2/src/providers/post-provider.ts
@@ -7,7 +7,7 @@ export class PostProvider {
   //server: string = "http://localhost/SprintIITest/IONIC4_CRUD_LOGINREGIS_PHP_MYSQL/server_api/"; // default
   // if you test in real device "http://localhost" change use the your IP
 
-  server: string = "http://localhost:8080/Proyecto-Software/server_api/"; //Equipo Alejandro
+  server: string = "http://localhost/Proyecto-Software/server_api/"; //Equipo Alejandro
   //server: string = "http://localhost/Proyecto-Software/server_api/";    //Equipo Diego
   constructor(public http: Http) {}
 
--- server_api/proses-api.php
+++ server_api/proses-api.php
@@ -1,50 +1,37 @@
 <?php
-
   header('Access-Control-Allow-Origin: *');
   header("Access-Control-Allow-Credentials: true");
   header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
   header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
   header("Content-Type: application/json; charset=utf-8");
-
   include "library/config.php";
   
   $postjson = json_decode(file_get_contents('php://input'), true);
   $today    = date('Y-m-d');
-
-
   if($postjson['aksi']=='add'){
-
   	$query = mysqli_query($mysqli, "INSERT INTO master_customer SET
   		name_customer = '$postjson[name_customer]',
   		desc_customer = '$postjson[desc_customer]',
   		created_at	  = '$today'  
   	");
-
   	$idcust = mysqli_insert_id($mysqli);
-
   	if($query) $result = json_encode(array('success'=>true, 'customerid'=>$idcust));
   	else $result = json_encode(array('success'=>false));
-
   	echo $result;
-
   }
   //AZTUALIZAR ESTADO DE DISPOSITIVO------------------------------------------------------------------------------------------------------------------
   elseif ($postjson['aksi']=='actualizardispositivo') {
     $id_dispositivo = $postjson['Dnum'];
     $status_dispositivo = $postjson['DValue'];
-
     $query = mysqli_query($mysqli,
     "UPDATE TabletStatus
     SET StatusDispositivo = $status_dispositivo
     WHERE Id_Dispositivo = $id_dispositivo");
-
     if($query) $result = json_encode(array('success'=>true, 'result'=>'success'));
     else $result = json_encode(array('success'=>false, 'result'=>'error'));
-
     echo $result;
   }
   
-
   //EMITIR VOTO------------------------------------------------------------------------------------------------------------------
   elseif ($postjson['aksi']=='emitirvoto') {
     //$id_voto = $postjson['id_Voto'];
@@ -54,43 +41,32 @@
     $id_diputados_parlacen = $postjson['id_diputados_parlacen'];
     $id_diputados_distrito= $postjson['id_diputados_distrito'];
     $id_diputados_lista = $postjson['id_diputados_lista'];
-
     $query = mysqli_query($mysqli,
     "INSERT INTO Voto_Emitido(id_centro, id_presidente_vicepresidente, id_alcalde, id_diputados_parlacen, id_diputados_distrito, id_diputados_lista)
      VALUES ($id_centro,$id_presidente_vicepresidente,$id_alcalde,$id_diputados_parlacen,$id_diputados_distrito,$id_diputados_lista)");
-
     if($query) $result = json_encode(array('success'=>true, 'result'=>'success'));
     else $result = json_encode(array('success'=>false, 'result'=>'error'));
-
     echo $result;
   }
-
   //ACTUALIZAR VOTANTE------------------------------------------------------------------------------------------------------------------
   elseif($postjson['aksi']=='update'){
   	$query = mysqli_query($mysqli, "UPDATE Votante SET 
   		Nombre='$postjson[Nombre]',
   		Empadronado='$postjson[Empadronado]' 
       WHERE DPI='$postjson[DPI]'");
-
   	if($query) $result = json_encode(array('success'=>true, 'result'=>'success'));
   	else $result = json_encode(array('success'=>false, 'result'=>'error'));
-
   	echo $result;
   }
  //-----------------------------------------------------------------------------------------------------------------------------------
-
   //ELIMINAR VOTANTE------------------------------------------------------------------------------------------------------------------
   elseif($postjson['aksi']=='delete'){
   	$query = mysqli_query($mysqli, "DELETE FROM Votante WHERE DPI='$postjson[DPI]'");
-
   	if($query) $result = json_encode(array('success'=>true, 'result'=>'success'));
   	else $result = json_encode(array('success'=>false, 'result'=>'error'));
-
   	echo $result;
-
   }
   //-----------------------------------------------------------------------------------------------------------------------------------
-
   //EN ESTE QUERY CON PHP SE OBTIENE EL VOTANTE CON EL DPI INGRESADO DESDE EL HTML-----------------------------------------------------
   elseif($postjson['aksi']=='getVotanteDPI'){
     $DPI = $postjson['DPI'];
@@ -101,9 +77,7 @@
      INNER JOIN Centro c on m.Codigo_Centro = c.Id_Centro
      WHERE v.DPI = $DPI
     ");
-
   	$check = mysqli_num_rows($query);
-
     if($check>0){
       $data = mysqli_fetch_array($query);
       $datauser = array(
@@ -113,18 +87,14 @@
         'Mesa' => $data['codigoMesa'],
         'Centro' => $data['Id_Centro'],
       );
-
       $result = json_encode(array('success'=>true, 'result'=>$datauser));  
-
     }else{
       $result = json_encode(array('success'=>false, 'msg'=>'Votante no encontrado'));
       
     }
     echo $result;
-
   }
   //-----------------------------------------------------------------------------------------------------------------------------------
-
   //SE OBTIENEN TODOS LOS VOTANTES DE LA BASE DE DATOS CON EL LIMITE ESPECIFICADO EN HOME.JS-------------------------------------------
   elseif($postjson['aksi']=='getdata'){
   	$data = array();
@@ -135,27 +105,20 @@
     INNER JOIN Centro c on m.Codigo_Centro = c.Id_Centro
     ORDER BY v.DPI DESC 
     ");
-
   	while($row = mysqli_fetch_array($query)){
-
   		$data[] = array(
   			'DPI' => $row['DPI'],
   			'Nombre' => $row['Nombre'],
   			'Empadronado' => $row['Empadronado'],
         'Mesa' => $row['codigoMesa'],
         'Centro' => $row['Id_Centro'],
-
   		);
   	}
-
   	if($query) $result = json_encode(array('success'=>true, 'result'=>$data));
   	else $result = json_encode(array('success'=>false));
-
   	echo $result;
-
   }
   //-----------------------------------------------------------------------------------------------------------------------------------
-
   //LOGIN------------------------------------------------------------------------------------------------------------------------------
   elseif($postjson['aksi']=="login"){
     $password = $postjson['password'];
@@ -166,7 +129,6 @@
      WHERE Clave='$password' AND Nombre='$Nombre'");
     
     $check = mysqli_num_rows($query);
-
     if($check>0){
       $data = mysqli_fetch_array($query);
       $datauser = array(
@@ -174,7 +136,6 @@
         'Nombre' => $data['Nombre'],
         'Clave' => $data['Clave']
       );
-
       $result = json_encode(array('success'=>true, 'result'=>$datauser));  
     }else{
       $result = json_encode(array('success'=>false, 'msg'=>'Cuenta no registrada'));
@@ -201,7 +162,6 @@
         'Mesa' => $data['Mesa'],
         'Centro' => $data['Centro']
       );
-
       $result = json_encode(array('success'=>true, 'result'=>$datauser));  
     }else{
       $result = json_encode(array('success'=>false, 'msg'=>'Query malo'));
@@ -223,7 +183,6 @@
       $datauser = array(
         'Nombre' => $data['Nombre']
       );
-
       $result = json_encode(array('success'=>true, 'result'=>$datauser));  
     }else{
       $result = json_encode(array('success'=>false, 'msg'=>'DPI no registrado'));
@@ -242,7 +201,6 @@
      FROM Presidente 
      WHERE Clave='$password' AND Nombre='$Nombre'");
     $check = mysqli_num_rows($query);
-
     if($check>0){
       $data = mysqli_fetch_array($query);
       $datauser = array(
@@ -250,7 +208,6 @@
         'Nombre' => $data['Nombre'],
         'Clave' => $data['Clave']
       );
-
       $result = json_encode(array('success'=>true, 'result'=>$datauser));  
     }else{
       $result = json_encode(array('success'=>false, 'msg'=>'Unregister Account'));
@@ -258,9 +215,7 @@
     }
     echo $result;
   }
-
   //---------------------------------------------------------------------------------------------------------------------------------
-
   /*elseif($postjson['aksi']=="register"){
     $password = md5($postjson['password']);
     $query = mysqli_query($mysqli, "INSERT INTO master_user SET
@@ -268,12 +223,8 @@
       password = '$password',
       status   = 'y'
     ");
-
     if($query) $result = json_encode(array('success'=>true));
     else $result = json_encode(array('success'=>false, 'msg'=>'error, please try again'));
-
     echo $result;
   }*/
-
-
 ?>
