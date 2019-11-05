4f56c67a03f9917c9cd5ea69e135cbb6c23953f2
Alejandro Tejada <tej17584@uvg.edu.gt>
2019-11-04 18:17:36 -0600

    Pruebas De Gui Katalon Studio

---
+++ PruebasAutomáticas/Pruebas GUI/Sprint 7/Test Suites/New Test Suite.ts
@@ -0,0 +1,25 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>New Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>142db248-44a0-44a3-8675-1d8017eb642c</testSuiteGuid>
+   <testCaseLink>
+      <guid>8b017d7d-8e4f-4c2b-9d81-63ed4cac1a07</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login Exitoso</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>3cb3f449-baca-4e0b-8445-b6c69f86ba07</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login Exitoso</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
