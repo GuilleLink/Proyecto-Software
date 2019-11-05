45f14742ccdbd6e7f2507b2ad3f3626ac3bad3d9
Andres Urizar <uri17632@uvg.edu.gt>
2019-11-05 17:46:26 -0600

    Ya muestra candidato de presidente y alcalde en
    Muestra estos datos en pagina de confirmacion voto

--- InterfazVotanteV2/InterfazVotanteV2/src/app/pages/confirmacion-voto/confirmacion-voto.module.ts
+++ InterfazVotanteV2/InterfazVotanteV2/src/app/pages/confirmacion-voto/confirmacion-voto.module.ts
@@ -6,7 +6,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
 
 import { ConfirmacionVotoPage } from './confirmacion-voto.page';
-
+import { ComponentsModule } from '../../components/components.module';
 const routes: Routes = [
   {
     path: '',
@@ -19,7 +19,8 @@ const routes: Routes = [
     CommonModule,
     FormsModule,
     IonicModule,
-    RouterModule.forChild(routes)
+    RouterModule.forChild(routes),
+    ComponentsModule
   ],
   declarations: [ConfirmacionVotoPage]
 })
--- InterfazVotanteV2/InterfazVotanteV2/src/app/pages/confirmacion-voto/confirmacion-voto.page.ts
+++ InterfazVotanteV2/InterfazVotanteV2/src/app/pages/confirmacion-voto/confirmacion-voto.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
+import { InfoService, TaskI, TaskAl, TaskDip } from '../../services/info.service';
 
 
 @Component({
@@ -15,21 +16,40 @@ export class ConfirmacionVotoPage implements OnInit {
   id_lista: string;
   id_distrito: string;
   id_parlacen: string;
+  presidente: TaskI;
+  Pname: string;
+  Ppartido: string;
+  PURL: string;
+  PidVoto: Int16Array;
+  alcalde: TaskAl;
+  Alname: string;
+  Alpartido: string;
+  AlURL: string;
+  AlidVoto: Int16Array;
 
-  constructor(private actRoute: ActivatedRoute) { }
+  constructor(private actRoute: ActivatedRoute,
+    private infoservice: InfoService) { }
+  
 
+  //const promise = new Promise()  
   ngOnInit() {
-  }
-
-  ionViewWillEnter(){
-    
+     //console.log("Llego al inicio a obtener los datos");
     this.actRoute.params.subscribe((data: any) =>{
-      this.id_presidente = data.presidente;
-      this.id_alcalde = data.alcalde;
-      this.id_lista = data.lista;
-      this.id_distrito =  data.distrito;
-      this.id_parlacen = data.parlacen;
-      console.log(data);
+        this.id_presidente = data.presidente;
+        this.id_alcalde = data.alcalde;
+        this.id_lista = data.lista;
+        this.id_distrito =  data.distrito;
+        this.id_parlacen = data.parlacen;
+        console.log("Obtuvo data de cantidatos"+data);
+    });
+    // console.log("llego a la parte de hacer la card");
+    // console.log(this.id_presidente);
+    this.infoservice.getAlcalde(this.id_alcalde).subscribe(res =>{
+      this.alcalde = res;
+      this.Alname = this.alcalde.alcalde;
+      this.Alpartido = this.alcalde.partido;
+      this.AlURL = this.alcalde.URL;
+      this.AlidVoto = this.alcalde.id_voto;
     });
   }
 
